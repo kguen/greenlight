368a7dd8df06632b87d2e781ab3f832123c74d4f
kguen <nanhkhoa460@gmail.com>
2021-11-13 18:11:20 +0700

    chapter 1+2

---
+++ cmd/api/routes.go
@@ -0,0 +1,14 @@
+package main
+
+import "github.com/go-chi/chi/v5"
+
+func (app *application) routes() *chi.Mux {
+	r := chi.NewRouter()
+
+	r.Route("/v1", func(r chi.Router) {
+		r.Get("/healthcheck", app.healthCheckHandler)
+		r.Post("/movies", app.createMovieHandler)
+		r.Get("/movies/{id}", app.showMovieHandler)
+	})
+	return r
+}
